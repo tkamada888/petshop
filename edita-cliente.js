23a27886fb653b09c00882f64db04a9596177516
Thiago <thiago.kamada@gmail.com>
2021-01-08 16:13:01 +0900

    CRUD with vanilla js and nodejs server

---
+++ edita-cliente.js
@@ -0,0 +1,46 @@
+const pegaURL = new URL(window.location)
+
+const id = pegaURL.searchParams.get('id')
+
+const inputCPF = document.querySelector('[data-cpf]')
+const inputNome = document.querySelector('[data-nome]')
+
+detalhaCliente(id).then( dados => {
+    inputCPF.value = dados[0].cpf 
+    inputNome.value = dados[0].nome
+})
+
+const formEdicao = document.querySelector('[data-form]')
+
+const alerta = (classe, mensagem) => { 
+    const linha = document.createElement('tr');
+
+    const conteudoLinha = `
+    <div class="${classe}">${mensagem}</div>
+    
+`
+  
+    linha.innerHTML = conteudoLinha;
+    return linha;
+} 
+formEdicao.addEventListener('submit', event => { 
+    event.preventDefault()
+        
+    editaCliente(id, inputCPF.value, inputNome.value)
+    .then( resposta => { 
+        if( resposta.status === 200){
+            formEdicao.appendChild(alerta(
+                "alert alert-success",
+                "CLIENTE EDITADO COM SUCESSO !",
+            ))
+        } else { 
+            formEdicao.appendChild(alerta(
+                "alert alert-warning",
+                "O CLIENTE NÃO PODE SER EDITADO !"
+            ))
+        }
+    })
+    
+    
+
+})
---
+++ listagem-cliente.js
@@ -0,0 +1,33 @@
+  const removeCliente = id => {
+    console.log(id)
+    if (confirm("Deseja deletar o cliente?")){
+      deletaCliente(id);
+    }
+  }
+  
+  const exibeCliente = (cpf, nome, id) => {
+  const linha = document.createElement("tr");
+
+  const conteudoLinha = `
+      
+      <td>${cpf}</td>
+      <td>${nome}</td>
+      <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
+      <a href="edita-clientes.html?id=${id}"><button type="button" class="btn btn-info">Editar</button></a>
+  `;
+
+  linha.innerHTML = conteudoLinha;
+
+  return linha;
+};
+
+const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+
+listarClientes().then(clientes => {
+  clientes.forEach(indice => {
+    corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id));
+  });
+});
+
+
+
